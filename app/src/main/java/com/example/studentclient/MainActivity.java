8b1093a9171125c1ab2061df4ecd13a478aa0670
dengzhiwen <litterboydeng@gmail.com>
2019-12-15 12:34:50 +0800

    viewPager completed

--- app/src/main/java/com/example/studentclient/MainActivity.java
+++ app/src/main/java/com/example/studentclient/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.studentclient;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -9,17 +11,73 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private BottomNavigationView navigationView;
+    private ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         toolbar = (Toolbar) findViewById(R.id.tool_bar);
         setSupportActionBar(toolbar);
+
+        navigationView = (BottomNavigationView)findViewById(R.id.bottom_menu);
+        viewPager = (ViewPager)findViewById(R.id.main_viewpager);
+
+        BottomAdapter adapter = new BottomAdapter(getSupportFragmentManager());
+        adapter.addFragment(new HomeFragment());
+        adapter.addFragment(new TalkFragment());
+        adapter.addFragment(new PersonalFragment());
+        viewPager.setAdapter(adapter);
+
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.bottom_home:
+                        viewPager.setCurrentItem(0);
+                        break;
+                    case R.id.talk_area:
+                        viewPager.setCurrentItem(1);
+                        break;
+                    case R.id.personal_info:
+                        viewPager.setCurrentItem(2);
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                navigationView.getMenu().getItem(position).setChecked(true);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
     }
 
 
